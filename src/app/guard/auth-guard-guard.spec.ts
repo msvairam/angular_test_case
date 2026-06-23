80b946fe5a32058767e1c4d00aebe7d689671295
msvairam <mutvairam@gmail.com>
2026-06-23 21:50:23 +0530

    vi.fn() vs vi.mock() vs vi.spyOn()

--- src/app/guard/auth-guard-guard.spec.ts
+++ src/app/guard/auth-guard-guard.spec.ts
@@ -4,8 +4,12 @@ import { CanActivateFn, provideRouter } from '@angular/router';
 
 import { authGuardGuard } from './auth-guard-guard';
 import { AuthStore } from '../store/auth.store';
-import { Mock, Mocked } from 'vitest';
+import { Mocked, test } from 'vitest';
 import { RouterTestingHarness } from '@angular/router/testing';
+
+const calculator = {
+  add: (a: number, b: number) => a + b,
+};
 
 @Component({
   template: `<h1>Login Page</h1>`,
@@ -25,7 +29,7 @@ describe('authGuardGuard', () => {
 
   async function setup(isAuthentication: boolean) {
     const authStore = {
-      isAuthentication:  vi.fn().mockReturnValue(isAuthentication)
+      isAuthentication: vi.fn().mockReturnValue(isAuthentication),
     } as Mocked<AuthStore>;
 
     TestBed.configureTestingModule({
@@ -64,5 +68,45 @@ describe('authGuardGuard', () => {
     await harness.navigateByUrl('/protected', ProtectedPage);
 
     expect(harness.routeNativeElement?.textContent).toContain('Protected Page');
+  });
+
+  test('spiecs method exection', () => {
+    // Create the spy
+    const addSpy = vi.spyOn(calculator, 'add');
+
+    const result = calculator.add(3, 2);
+
+    expect(result).toBe(5);
+    expect(addSpy).toHaveBeenCalled();
+    expect(addSpy).toHaveBeenCalledWith(3, 2);
+
+    // Clean up (restores original un-spied function)
+    addSpy.mockRestore();
+  });
+
+  test('spies and alters behaviours', () => {
+    const addSpy = vi.spyOn(calculator, 'add').mockReturnValue(100);
+
+    expect(calculator.add(2, 4)).toBe(100); // Forces return value
+
+    // Provide a completely fake temporary implementation
+    addSpy.mockImplementation((a, b) => a * b);
+    expect(calculator.add(2, 3)).toBe(6);
+
+    expect(addSpy).toHaveBeenCalled();
+    expect(addSpy).toHaveBeenCalledWith(2, 4);
+  });
+
+  test('standalone spy function', () => {
+    const onClickSpy = vi.fn();
+
+    onClickSpy('on_click');
+
+    expect(onClickSpy).toHaveBeenCalledOnce();
+    expect(onClickSpy).toHaveBeenCalledWith('on_click');
+
+    const result = vi.fn().mockReturnValue(100);
+
+    expect(result()).toBe(100);
   })
 });
